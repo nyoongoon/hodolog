0fabd66e6d0ed77ce1a18f37e6aa2306918614b3
nyoongoon <nyoongoon@naver.com>
2024-01-03 06:09:31 +0900

    DTO 추가 PostCreate

--- src/main/java/com/hodolog/hodolog/controller/PostController.java
+++ src/main/java/com/hodolog/hodolog/controller/PostController.java
@@ -1,14 +1,19 @@
 package com.hodolog.hodolog.controller;
 
+import com.hodolog.hodolog.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class PostController {
 
     @PostMapping("/posts")
-    public String post(){
+    public String post(PostCreate params){
+        log.info("params={}", params.toString());
         return "Hello World";
     }
 }
---
+++ src/main/java/com/hodolog/hodolog/request/PostCreate.java
@@ -0,0 +1,10 @@
+package com.hodolog.hodolog.request;
+
+import lombok.ToString;
+
+@ToString
+public class PostCreate {
+
+    public String title;
+    public String content;
+}
