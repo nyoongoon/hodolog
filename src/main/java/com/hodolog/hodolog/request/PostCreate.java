cca44eb638e18a83f33832973edcaf9a7e1eb13b
nyoongoon <nyoongoon@naver.com>
2024-01-03 06:10:50 +0900

    PostCreate Setter 추가 - controller에서 toString의 결과가 null 이었음

--- src/main/java/com/hodolog/hodolog/request/PostCreate.java
+++ src/main/java/com/hodolog/hodolog/request/PostCreate.java
@@ -2,9 +2,24 @@ package com.hodolog.hodolog.request;
 
 import lombok.ToString;
 
-@ToString
 public class PostCreate {
 
     public String title;
     public String content;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "PostCreate{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
