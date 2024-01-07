b59af783044908be4fcada4e062e8fe0264d0cc6
nyoongoon <nyoongoon@naver.com>
2024-01-08 06:03:41 +0900

    error handler return data josn형식으로 변환

--- src/main/java/com/hodolog/hodolog/controller/ExceptionController.java
+++ src/main/java/com/hodolog/hodolog/controller/ExceptionController.java
@@ -2,18 +2,32 @@ package com.hodolog.hodolog.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @ControllerAdvice
+@ResponseBody // return data를 jsom으로 변환.;
 public class ExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(Exception.class)
-    public void exceptionHandler(Exception e) {
-        log.info("exceptionHandler error={}", e);
-        System.out.println("하하하");
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+
+    public Map<String, String> invalidRequestHandler(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getFieldError();
+        String field = fieldError.getField();
+        String message = fieldError.getDefaultMessage();
+
+        Map<String, String> response = new HashMap<>();
+        response.put(field, message);
+        return response;
+
     }
 }
