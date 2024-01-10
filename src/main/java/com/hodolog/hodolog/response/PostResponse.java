cb80a2869fa64eb30c859a54a0ae856b638a83c5
nyoongoon <nyoongoon@naver.com>
2024-01-11 05:55:54 +0900

    엔티티-> DTO변환을 어느 레이어에서 할지 고민

--- src/main/java/com/hodolog/hodolog/response/PostResponse.java
+++ src/main/java/com/hodolog/hodolog/response/PostResponse.java
@@ -13,4 +13,11 @@ public class PostResponse {
     private final Long id;
     private final String title;
     private final String content;
+
+    @Builder
+    public PostResponse(Long id, String title, String content) {
+        this.id = id;
+        this.title = title.substring(0, Math.min(title.length(), 10));
+        this.content = content;
+        }
 }
