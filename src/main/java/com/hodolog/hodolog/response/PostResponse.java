91a9a4a86d9b8b903f26455798d07bd0820ae29d
nyoongoon <nyoongoon@naver.com>
2024-01-15 05:51:29 +0900

    PostResonse 생성자 오버로딩 : entity -> dto

--- src/main/java/com/hodolog/hodolog/response/PostResponse.java
+++ src/main/java/com/hodolog/hodolog/response/PostResponse.java
@@ -1,5 +1,6 @@
 package com.hodolog.hodolog.response;
 
+import com.hodolog.hodolog.domain.Post;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,13 @@ public class PostResponse {
     private final Long id;
     private final String title;
     private final String content;
+
+    // 생성자 오버로딩
+    public PostResponse(Post post){
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+    }
 
     @Builder
     public PostResponse(Long id, String title, String content) {
