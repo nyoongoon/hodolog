594a49ca1d4916f5ea6483399af1b9038495d5ac
nyoongoon <nyoongoon@naver.com>
2024-04-09 06:31:10 +0900

    ExceptionController(ControllerAdvice)에서 커스텀 예외 핸들링 설정 - http응답값은 @ResponseStatus()로, json 형태의 코드와 메시지는 바디에 담아서 리턴

--- src/test/java/com/hodolog/hodolog/controller/PostControllerTest.java
+++ src/test/java/com/hodolog/hodolog/controller/PostControllerTest.java
@@ -222,11 +222,14 @@ class PostControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", 1L)
                         .contentType(MediaType.APPLICATION_JSON)
                 )
-                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.status().isNotFound()) // status code는 @ResponseStatus(HttpStatus.NOT_FOUND) 로 설정 & 나머지 json 형태의 코드와 메시지는 바디에 담아서 보냄..!
                 .andDo(MockMvcResultHandlers.print());
         // 커스텀 예외를 서비스에 추가하고, 컨트롤러에서 추가 작업을 하지 않는 경우,
         // -> PostNotFound 예외가 발생하면서 서버오류가 발생하긴 하나
         // 예상하는 Http 응답값을 받을 수가 없음 !
+
+        // ExceptionController에서 처리한 것처럼 json 형태로 코드와 메시지가 내려올 것을 기대
+        // -> 정형화된 데이터 내려주는 것으로 변경 필요
     }
 
 }
