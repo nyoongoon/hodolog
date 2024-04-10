e645a4c9521edeea2a82a6c63a605c4bd14bb092
nyoongoon <nyoongoon@naver.com>
2024-04-11 08:29:43 +0900

    restdoc 연동 중

--- src/test/java/com/hodolog/hodolog/controller/PostControllerDocTest.java
+++ src/test/java/com/hodolog/hodolog/controller/PostControllerDocTest.java
@@ -1,41 +1,52 @@
 package com.hodolog.hodolog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hodolog.hodolog.domain.Post;
 import com.hodolog.hodolog.repository.PostRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.hodolog.hodolog.request.PostCreate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * API 문서 만들기 : REST DOC
+ * asciidoc으로 restdoc 만들기
+ *
+ * gradle의 asciidoctor task는 test 실행 후 build/에 snippet, index.html을 생성함.
+ * 그리고 build 시 bootJar가 실행되면서 build/에 있는 index.html을 /resoures/static/docs로 복사함.
+ *
+ */
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.hodolman.com", uriPort = 443)
 @ExtendWith(RestDocumentationExtension.class)
 public class PostControllerDocTest {
+
+    @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Autowired
     private PostRepository postRepository;
 
-    @BeforeEach
-    void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
-
     @Test
-    @DisplayName("아무개")
+    @DisplayName("글 단건 조회")
     void test1() throws Exception {
         //given
         Post post = Post.builder()
@@ -44,9 +55,43 @@ public class PostControllerDocTest {
                 .build();
         postRepository.save(post);
         //expected
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", 1L).accept(MediaType.APPLICATION_JSON))
+//        this.mockMvc.perform(MockMvcRequestBuilders.get("/posts/{postId}", 1L).accept(MediaType.APPLICATION_JSON) // restdoc때문에 builders변경
+        this.mockMvc.perform(RestDocumentationRequestBuilders.get("/posts/{postId}", 1L).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("index"));
+                .andDo(document("post-inquiry",
+                        RequestDocumentation.pathParameters(
+                                RequestDocumentation.parameterWithName("postId").description("게시글ID")
+                        ),
+                        PayloadDocumentation.responseFields(
+                                PayloadDocumentation.fieldWithPath("id").description("게시글 ID"),
+                                PayloadDocumentation.fieldWithPath("title").description("제목"),
+                                PayloadDocumentation.fieldWithPath("content").description("내용")
+                        )
+                ));
+    }
+
+    @Test
+    @DisplayName("글 등록")
+    void test2() throws Exception {
+        //given
+        PostCreate request = PostCreate.builder()
+                .title("제목입니다.")
+                .content("내용입니다.")
+                .build();
+        String json = objectMapper.writeValueAsString(request);
+
+        //expected
+        this.mockMvc.perform(RestDocumentationRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON) // accept() 와 contentType() ?? -> 비교해보기
+                        .content(json))
+                .andExpect(status().isOk())
+                .andDo(document("post-create",
+                        PayloadDocumentation.requestFields(
+                                PayloadDocumentation.fieldWithPath("title").description("제목"),
+                                PayloadDocumentation.fieldWithPath("content").description("내용")
+                        )
+                ));
 
 
     }
