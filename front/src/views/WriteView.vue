3aa57acb4312448d6749d150773f59437a04e8e7
nyoongoon <nyoongoon@naver.com>
2024-04-15 07:00:57 +0900

    element-plus, bootstrap 의존성 추가

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <el-input type="text" placeholder="제목을 입력해주세요"/>
+  </div>
+  <div class="mt-2">
+    <el-input type="textarea" rows="15"/>
+  </div>
+  <div class="mt-2">
+    <el-button type="primary">글 작성 완료</el-button>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
