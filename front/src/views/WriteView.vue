22ee91f30fce72c98b44fbc46d5e92949581d1a1
nyoongoon <nyoongoon@naver.com>
2024-04-16 06:11:43 +0900

    cors이슈 백엔드 처리방안

--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
+import {ref} from "vue";
+import axios from "axios"
+
+const title = ref("");
+const content = ref("");
+
+const write = function () {
+  axios.post("http://localhost:8080/posts", {
+    title: title.value,
+    content: content.value
+  });
+}
 </script>
 
 <template>
   <div>
-    <el-input type="text" placeholder="제목을 입력해주세요"/>
+    <el-input v-model="title" type="text" placeholder="제목을 입력해주세요"/>
   </div>
   <div class="mt-2">
-    <el-input type="textarea" rows="15"/>
+    <el-input v-model="content" type="textarea" rows="15"/>
   </div>
   <div class="mt-2">
-    <el-button type="primary">글 작성 완료</el-button>
+    <el-button type="primary" @click="write()">글 작성 완료</el-button>
   </div>
 
 
---
+++ src/main/java/com/hodolog/hodolog/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.hodolog.hodolog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    /**
+     * CORS 이슈 백엔드에서 처리 방법
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173/");
+    }
+}
