47157c8474755edaf9e2da2ba2d8eeed1ade763e
nyoongoon <nyoongoon@naver.com>
2024-06-10 12:55:00 +0900

    jwt deprecated 적용 & 프로퍼티에서 시크릿키 못가져오는 중..

--- src/main/java/com/hodolog/hodolog/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/hodolog/hodolog/config/jwt/JwtTokenProvider.java
@@ -3,10 +3,12 @@ package com.hodolog.hodolog.config.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
@@ -14,9 +16,11 @@ import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.util.WebUtils;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.List;
 
+@Slf4j
 @Component
 public class JwtTokenProvider {
     // Http 프로토콜에서 헤더에 포함 되는데, 어떤 key에 토큰을 줄건지 설정
@@ -27,17 +31,37 @@ public class JwtTokenProvider {
     private static final long REFRESH_TOKEN_EXPIRED_TIME = 1000 * 60 * 60 * 24; //밀리세컨드*초*분*시 == 24시간
     private static final String KEY_ROLES = "roles";
 
-//    private final UserDetailsService userDetailsService;
-
-    @Value("{spring.jwt.access-secret-key}")
+    @Value("{spring.jwt.access-secret-key}") //todo value값이 안가져와짐.
     private String accessSecretKey;
 
     @Value("{spring.jwt.refresh-secret-key}")
     private String refreshSecretKey;
 
-//    public JwtTokenProvider(UserDetailsService userDetailsService) {
-//        this.userDetailsService = userDetailsService;
-//    }
+    // 토큰 발급
+    public TokenDto getTokens(String userEmail, List<GrantedAuthority> roles) {
+        // 사용자의 권한정보를 저장하기 위한 클레임 생성
+        Claims claims = createClaims(userEmail, roles);
+
+        //Access Token & Refresh Token
+        String refreshToken = generateToken(claims, getSecretKey(JwtTokenType.REFRESH_TOKEN), REFRESH_TOKEN_EXPIRED_TIME);
+        String accessToken = this.getAccessTokenByRefreshToken(refreshToken);
+
+        return new TokenDto(userEmail, accessToken, refreshToken);
+    }
+
+    private SecretKey getSecretKey(JwtTokenType jwtTokenType) {
+        String secretKey;
+        if (jwtTokenType == JwtTokenType.ACCESS_TOKEN) {
+            secretKey = this.accessSecretKey;
+        } else {
+            secretKey = this.refreshSecretKey;
+        }
+
+        log.info("secretKey : {}", secretKey);
+
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
     /**
      * 토큰 생성
@@ -47,58 +71,46 @@ public class JwtTokenProvider {
      * @param expiredTime
      * @return
      */
-    private String generateToken(Claims claims, String secretKey, long expiredTime) {
+    private String generateToken(Claims claims, SecretKey secretKey, long expiredTime) {
         Date now = new Date();
         //jwt Token
         return Jwts.builder()
+                .signWith(secretKey)  // 시그니처 알고리즘, 비밀키 //todo base64 error..
                 .setClaims(claims) // 정보 저장
                 .setIssuedAt(now) // 토큰 발행 시간 정보
                 .setExpiration(new Date(now.getTime() + expiredTime)) // 토큰 만료 시간
-                .signWith(SignatureAlgorithm.HS256, secretKey)  // 시그니처 알고리즘, 비밀키 //todo base64 error..
                 .compact();
     }
 
+    /**
+     * @param userEmail
+     * @param roles
+     * @return
+     */
+    private Claims createClaims(String userEmail, List<GrantedAuthority> roles) {
+        return Jwts.claims()
+                .subject(userEmail)
+                .add(KEY_ROLES, roles)
+                .build();
+    }
 
     // 토큰 유효성 체크
-    public Claims parseClaims(String token, JwtTokenType jwtTokenType) {
+    private Claims parseClaims(String token, JwtTokenType jwtTokenType) {
         // 토큰 만료 경우 예외 발생
-        String secretKey;
-        if(jwtTokenType == JwtTokenType.ACCESS_TOKEN){
-            secretKey = this.accessSecretKey;
-        }else{ // REFRESH_TOKEN
-            secretKey = this.refreshSecretKey;
-        }
+        SecretKey secretKey = jwtTokenType == JwtTokenType.ACCESS_TOKEN ?
+                getSecretKey(JwtTokenType.ACCESS_TOKEN) : getSecretKey(JwtTokenType.REFRESH_TOKEN);
         try {
-            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload();
         } catch (ExpiredJwtException e) {
             return e.getClaims();
         }
     }
 
-    // 토큰 발급
-    public TokenDto getTokens(String userEmail, List<GrantedAuthority> roles) {
-        // 사용자의 권한정보를 저장하기 위한 클레임 생성
-        Claims claims = Jwts.claims().setSubject(userEmail);
-        claims.put(KEY_ROLES, roles); // 클레임은 키밸류
-
-        //Access Token & Refresh Token
-        String refreshToken = this.getRefreshTokenByClaims(claims);
-        String accessToken = this.getAccessTokenByRefreshToken(refreshToken);
-
-        return new TokenDto(userEmail, accessToken, refreshToken);
-    }
-
-    
-    public String getRefreshTokenByClaims(Claims claims) {
-        return this.generateToken(claims, this.refreshSecretKey, REFRESH_TOKEN_EXPIRED_TIME);
-    }
-
-    
     public String getAccessTokenByRefreshToken(String refreshToken) {
         // 사용자의 권한정보를 저장하기 위한 클레임 생성
         Claims claims = this.parseClaims(refreshToken, JwtTokenType.REFRESH_TOKEN);
         // 엑세스 토큰 재발급
-        return this.generateToken(claims, accessSecretKey, ACCESS_TOKEN_EXPIRED_TIME);
+        return this.generateToken(claims, getSecretKey(JwtTokenType.ACCESS_TOKEN), ACCESS_TOKEN_EXPIRED_TIME);
     }
 
     // 권한 얻기
@@ -122,7 +134,7 @@ public class JwtTokenProvider {
 //        return unauthenticated;
 //    }
 
-    
+
     public boolean validateToken(String token, JwtTokenType jwtTokenType) {
         if (!StringUtils.hasText(token)) {
             return false;
@@ -132,18 +144,17 @@ public class JwtTokenProvider {
         return !claims.getExpiration().before(new Date());
     }
 
-    
+
     public boolean validateAccessToken(String token) {
         return validateToken(token, JwtTokenType.ACCESS_TOKEN);
     }
 
-    
+
     public boolean validateRefreshToken(String token) {
         return validateToken(token, JwtTokenType.REFRESH_TOKEN);
     }
 
     // 헤더에서 엑세스 토큰 얻기
-    
     public String resolveAccessTokenByHeader(HttpServletRequest request) {
         String accessToken = request.getHeader(TOKEN_HEADER);
 
@@ -155,7 +166,6 @@ public class JwtTokenProvider {
     }
 
     // 쿠키에서 리프레시 토큰 얻기
-    
     public String resolveRefreshTokenByCookie(HttpServletRequest request) {
         Cookie refreshTokenCookie = WebUtils.getCookie(request, "refreshToken");
         if (refreshTokenCookie != null) {
@@ -164,7 +174,6 @@ public class JwtTokenProvider {
         return null;
     }
 
-    
     public void addAccessTokenToCookie(HttpServletResponse response, String accessToken) {
         Cookie accessTokenCookie = new Cookie("accessToken", accessToken);
         accessTokenCookie.setHttpOnly(true); // JavaScript에서 쿠키에 접근 불가능하도록 설정
@@ -172,12 +181,11 @@ public class JwtTokenProvider {
         response.addCookie(accessTokenCookie);
     }
 
-    
+
     public void addRefreshTokenToCookie(HttpServletResponse response, String refreshToken) {
         Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
         refreshTokenCookie.setHttpOnly(true); // JavaScript에서 쿠키에 접근 불가능하도록 설정
         refreshTokenCookie.setMaxAge(60 * 60 * 24 * 30); // 쿠키 유효 기간 설정 (예: 30일)
         response.addCookie(refreshTokenCookie);
     }
-
 }
