971f80492603028c9a54fe8052f5e99e65aa1ea0
nyoongoon <nyoongoon@naver.com>
2024-07-05 07:44:20 +0900

    axios 공통화하지 않아서 마무리

--- front/src/views/LoginView.vue
+++ front/src/views/LoginView.vue
@@ -1,19 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+import Login from '@/entity/user/Login'
+import axios from 'axios'
+
+// 클래스 데이터를 사용
+const state = reactive({
+  login: new Login()
+})
+
+function doLogin() {
+  console.log('>>', state.login)
+  axios.post('/api/auth/login', state.login)
+}
+</script>
 
 <template>
   <el-row>
     <el-col :span="10" :offset="7">
       <el-form label-position="top">
         <el-form-item label="이메일">
-          <el-input></el-input>
+          <el-input v-model="state.login.email"></el-input>
         </el-form-item>
 
         <el-form-item label="비밀번호">
-          <el-input type="password"></el-input>
+          <el-input type="password" v-model="state.login.password"></el-input>
         </el-form-item>
 
         <el-form-item>
-          <el-button type="primary" style="width: 100%">로그인</el-button>
+          <el-button type="primary" style="width: 100%" @click="doLogin()">로그인</el-button>
         </el-form-item>
       </el-form>
     </el-col>
